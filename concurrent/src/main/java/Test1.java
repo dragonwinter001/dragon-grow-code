1511c040edca5082bc93684e53447191bb75546c
陈浩杰 <15533409978@163.com>
2022-04-23 00:35:13 +0800

    add:first commit

---
+++ concurrent/src/main/java/Test1.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName: Test1
+ * @Description: 测试
+ * @Date: 2022/4/23 00:03
+ * @Author: chj
+ * @Version: 1.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
