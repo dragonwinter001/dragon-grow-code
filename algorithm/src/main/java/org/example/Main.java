3b18fc50e0a58125f8b23ac51281a84da97fb600
chenhaojie <chen_accept@163.com>
2023-09-05 14:49:37 +0800

    add:编程导航study

---
+++ algorithm/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @ClassName: ${NAME}
+ * @Description: TODO
+ * @Date: ${DATE} ${HOUR}:${MINUTE}
+ * @Author: chj
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
